3726b157b0021670bdfb274cd3bb74ef5d923bc4
collinlidstrom <collinlidstrom@gmail.com>
2022-01-03 00:11:22 -0600

    - add new data - add spacing between buttons in vstack

--- SampleApp/ContentView.swift
+++ SampleApp/ContentView.swift
@@ -14,18 +14,31 @@ struct ContentView: View {
     var body: some View {
         
         
-        ForEach(0..<model.people.count){ index in
-            
-            
-            
-            Button {
-                print("Hey Oliver")
-            } label: {
-                ZStack {
-                    Rectangle().frame(width: 100, height: 100, alignment: .center).foregroundColor(.black)
-                    Text(model.people[index].firstName!).foregroundColor(.white)
-                    
-
+        VStack(spacing: 45.0){
+            ForEach(0..<model.people.count){ index in
+                
+                
+                
+                Button {
+                    print("Hey Oliver")
+                } label: {
+                    ZStack {
+                        Rectangle().frame(width: 100, height: 100, alignment: .center).foregroundColor(.black)
+                        VStack{
+                            Text(model.people[index].firstName ?? "Name unknown")
+                            Text(model.people[index].lastName!)
+                            if model.people[index].age == nil {
+                                Text("Age Unknown")
+                            }else{
+                                Text("Age: \(model.people[index].age!)")
+                            }
+                            
+                        }
+                        .foregroundColor(.white)
+                        
+                        
+                        
+                    }
                 }
             }
         }
